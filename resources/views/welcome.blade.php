70477a8ea27d09745bb324f32506975b7664b0e0
Barmarthi <barbarathimoteo@hotmail.com>
2024-04-04 10:53:39 -0300

    Criação do layout base, menu e alteração da home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,9 +92,15 @@
 
                                         <div class="pt-3 sm:pt-5 lg:pt-0">
                                             <h2 class="text-xl font-semibold text-black dark:text-white">Teste</h2>
-
-                                            <p class="mt-4 text-sm/relaxed">
-                                                Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
+                                            <button class="btn">Button</button>
+<button class="btn btn-neutral">Neutral</button>
+<button class="btn btn-primary">Primary</button>
+<button class="btn btn-secondary">Secondary</button>
+<button class="btn btn-accent">Accent</button>
+<button class="btn btn-ghost">Ghost</button>
+<button class="btn btn-link">Link</button>
+                                            <p class="mt-4 text-sm/relaxed bg-red-500">
+                                                sla mano faz a aula ai
                                             </p>
                                         </div>
                                     </div>
