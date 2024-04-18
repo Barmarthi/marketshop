e2116d3e4bdacd46b400ed96196a3b3277860761
Barmarthi <barbarathimoteo@hotmail.com>
2024-04-18 10:45:54 -0300

    Tela de cadastro

--- resources/views/components/layout_base.blade.php
+++ resources/views/components/layout_base.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css','resources/js/app.js'])
     <title>{{$titulo ?? 'MarketShop'}}</title>
 </head>
 <body data-theme="light" class="bg-gray-200 min-h-screen">
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
 });
 
-Route::view('/home','home');
+Route::view('/','home');
+Route::view('/cria-conta','cria-conta');
 Route::view('/teste','teste');
