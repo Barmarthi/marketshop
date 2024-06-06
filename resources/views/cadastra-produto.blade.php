4c7c248d7db8df06e6ef6cff4ed7d118d9f8bd8b
Barmarthi <barbarathimoteo@hotmail.com>
2024-06-06 10:51:05 -0300

    Cadastro produtos

--- resources/views/cadastra-produto.blade.php
+++ resources/views/cadastra-produto.blade.php
@@ -1,9 +1,10 @@
 <x-layout_base>
-    <div class="relative bg-[url('https://png.pngtree.com/background/20220731/original/pngtree-white-modern-background-free-download-hd-picture-image_1912983.jpg')] bg-no-repeat bg-cover">
+    <div class="relative bg-[url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5236376f-305d-42da-8e2d-48455360a090/de12wow-49b408f7-487d-425b-89fd-1d0a31b78d28.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzUyMzYzNzZmLTMwNWQtNDJkYS04ZTJkLTQ4NDU1MzYwYTA5MFwvZGUxMndvdy00OWI0MDhmNy00ODdkLTQyNWItODlmZC0xZDBhMzFiNzhkMjgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.MaE4jfg5A2gymwpQftFJ8R7dvmnhs1dI2Apocu3nzqs')] bg-no-repeat bg-cover">
 <!-- Card Section -->
 <div class="max-w-4xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
     
-  <form>
+  <form method="POST" action ="{{route('salva-produto')}}" enctype="multipart/form-data">
+    @csrf
     <!-- Card -->
     <div class="bg-gradient-to-b from-pink-600 to-white rounded-xl ">
       <div class="relative bg-[url('https://preline.co/assets/svg/examples/abstract-bg-1.svg')] bg-no-repeat bg-cover bg-center">
@@ -43,7 +44,7 @@
               Nome do Produto
             </label>
 
-            <input id="af-submit-app-project-name" type="text" class="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Insira nome do projeto">
+            <input name="nome" id="af-submit-app-project-name" type="text" class="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Insira nome do projeto">
           </div>
 
           <div class="space-y-2">
@@ -51,7 +52,7 @@
               Preço
             </label>
 
-            <input id="af-submit-project-url" type="text" class="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Insira o preço">
+            <input name="valor" id="af-submit-project-url" type="text" class="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Insira o preço">
           </div>
 
           <div class="space-y-2">
@@ -60,13 +61,13 @@
             </label>
 
             <label for="af-submit-app-upload-images" class="group p-4 sm:p-7 block cursor-pointer text-center border-2 border-dashed border-gray-200 rounded-lg focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 dark:border-neutral-700">
-              <input id="af-submit-app-upload-images" name="af-submit-app-upload-images" type="file" class="sr-only">
+              <input name="foto" id="af-submit-app-upload-images" type="file" class="sr-only">
               <svg class="size-10 mx-auto text-gray-400 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2z"/>
                 <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
               </svg>
               <span class="mt-2 block text-sm text-gray-800 dark:text-neutral-200">
-                Procure em seus arquivos ou<span class="group-hover:text-blue-700 text-blue-600"> gfdgfd</span>
+                Procure em seus arquivos<span class="group-hover:text-blue-700 text-blue-600"> clicando aqui</span>
               </span>
               <span class="mt-1 block text-xs text-gray-500 dark:text-neutral-500">
                 Maximum file size is 2 MB
@@ -79,13 +80,13 @@
               Descrição
             </label>
 
-            <textarea id="af-submit-app-description" class="py-2 px-3 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" rows="6" placeholder="Uma descrição detalhada do produto"></textarea>
+            <textarea name="descricao" id="af-submit-app-description" class="py-2 px-3 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" rows="6" placeholder="Uma descrição detalhada do produto"></textarea>
           </div>
         </div>
         <!-- End Grid -->
 
         <div class="mt-5 flex justify-center gap-x-2">
-          <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+          <button type="submit" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
             Submit your project
           </button>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,24 @@ Route::post('/salva-usuario',function(Request $request){
 // ------------------------ Produtos
 Route::view('/cadastra-produto','cadastra-produto');
 
+
+Route::post('/salva-produto',function(Request $request){
+    //dd($request);
+
+    $produto = new Produto();
+    $produto ->name = $request->nome;
+    $produto ->descricao = $request->descricao;
+    $produto ->valor = $request->valor;
+
+
+    $file= $request->file('foto');
+    $foto= $file->store ('produtos', ['disk' => 'fotos']);
+
+
+    $produto->foto = $foto;
+    $produto->user_id = 1;
+
+    $produto->save();
+    dd("Salvo com Sucesso!!");
+
+})->name('salva-produto');
